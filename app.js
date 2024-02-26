2e4c8eb4c5142964ced056bf72f3e2b7d4a6479d
Neka Blair <ourblairfamily@gmail.com>
2024-02-26 15:55:42 -0600

    first commit

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
-console.log("HELLO PAPA PLATOON!")
 // Your function(s) should go here that will interact with the webpage or DOM
+let userInput = document.querySelector("input")
+let userNum = document.querySelector("#userNumber")
+let btn = document.querySelector("button")
+let output = document.querySelector("#output")
+let overBtn = document.querySelector("#over");
+let randNum = Math.floor(Math.random() * 100) + 1;
+
+let check = () => {
+    const userGuess = Number(userNum.value);
+    if (userGuess === randNum) {
+        output.innerHTML = "You guessed the right number!";
+    } else if (userGuess > randNum) {
+        output.textContent = "You are too high!"
+    } else {
+        output.textContent = "You are too low!"
+    } 
+}
+
+let reset = () => {
+    userInput.value = ""
+    userNum.value = ""
+    output.innerHTML = ""
+}
+
+btn.addEventListener("click", check);
+overBtn.addEventListener("click", reset);
